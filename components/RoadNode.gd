e14b97245ad57c11896eacf5bde98483e2a20ff3
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-11-26 11:58:32 -0500

    place node

---
+++ components/RoadNode.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name RoadNode
+
+
+var connections: Array
+
+func _init() -> void:
+    pass
+
+
+func _ready() -> void:
+    pass
--- components/tools/MultiTool.gd
+++ components/tools/MultiTool.gd
@@ -2,19 +2,38 @@ extends Node
 class_name MultiTool
 
 
+
+@export var roadnode_scene: PackedScene
+
+var click_position: Vector2
+
 func _init() -> void:
     pass
+    
 
 
 func _input(event: InputEvent) -> void:
     if event is InputEventMouseButton and event.is_pressed():
+
         @warning_ignore("unsafe_property_access")
         match event.button_index:
+
             MOUSE_BUTTON_LEFT:
                 @warning_ignore("unsafe_property_access")
                 print("Mouse left pressed at: ", event.position)
 
+                @warning_ignore("unsafe_property_access", "unsafe_call_argument")
+                place_scene(roadnode_scene, event.position)
+
 func _process(_delta: float) -> void:
     pass
 
+
+func place_scene(scene: PackedScene, position: Vector2) -> void:
+
+    var road_node: Node2D = scene.instantiate()
+
+    road_node.position = position
+
+    add_child(road_node)
 
