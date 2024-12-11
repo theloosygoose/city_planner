b7381e27e8c8c9c10520681492b2cc29773f1854
Gusti <gustihenry@Gustis-MacBook-Air.local>
2024-12-10 21:59:50 -0500

    only draw in draw mode?

--- components/tools/MultiTool.gd
+++ components/tools/MultiTool.gd
@@ -5,10 +5,14 @@ class_name MultiTool
 @export var CityData: Resource
 
 var click_position: Vector2
+var current_mode: ModeSetting.MODES 
 
 func _init() -> void:
-	pass
+	ModeSetting.mode_changed.connect(_mode_changed)
 
+
+func _mode_changed(mode: ModeSetting.MODES) -> void:
+	current_mode = mode
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
@@ -19,15 +23,12 @@ func _input(event: InputEvent) -> void:
 			MOUSE_BUTTON_LEFT:
 				print("Mouse left pressed at: ", get_global_mouse_position())
 
-				place_scene(roadnode_scene, get_global_mouse_position())
-
-
+				draw_road(roadnode_scene, get_global_mouse_position())
 
 func _process(_delta: float) -> void:
 	pass
 
-
-func place_scene(scene: PackedScene, c_position: Vector2) -> void:
+func draw_road(scene: PackedScene, c_position: Vector2) -> void:
 
 	var road_node: Node2D = scene.instantiate()
 
